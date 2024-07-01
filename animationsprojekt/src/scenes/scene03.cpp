2395976db5a9e57daf08fd94afc2591cfeb3d943
hensing1 <s88jlehm@uni-bonn.de>
2024-07-01 02:48:25 +0200

    Hörsaal modelliert

--- animationsprojekt/src/scenes/scene03.cpp
+++ animationsprojekt/src/scenes/scene03.cpp
@@ -1,10 +1,15 @@
 #include "scene.hpp"
 
-Scene03::Scene03() {}
+Scene03::Scene03() {
+    hoersaal.load("meshes/HS3.obj");
+}
 
 void Scene03::render(int frame, Program& program, Camera& camera) {
     camera.updateIfChanged();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    vec3 pos = vec3(0, 0, 0);
+    mat4 worldToClip = camera.projectionMatrix * camera.viewMatrix;
+    this->drawMesh(1.0f, pos, program, hoersaal, worldToClip);
 }
 
 Scene03::~Scene03() {}
