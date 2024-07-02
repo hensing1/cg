c58971bc19b766e30af750126cd620474355bb18
Florian Barth <s58fbart@uni-bonn.de>
2024-07-02 21:55:58 +0200

    Operations und darin Rotation hinzugefügt

--- animationsprojekt/src/scenes/scene.cpp
+++ animationsprojekt/src/scenes/scene.cpp
@@ -22,6 +22,20 @@ void Scene::drawMesh(float size, const vec3& pos, Program& program, Mesh& mesh,
     program.bind();
     mesh.draw();
     srand(42); // Reset random seed for static colors
+    operations = Operations();
 }
+
+//  NOTE: Nur zu Testzwecken -> später entfernen
+void Scene::drawMesh(float size, const vec3& pos, Program& program, Mesh& mesh,
+                     const mat4& worldToClip, const mat4& transformation) {
+    mat4 localToWorld = scale(translate(mat4(1.0f), pos), vec3(size));
+    program.set("uLocalToWorld", localToWorld);
+    program.set("uLocalToClip", worldToClip * localToWorld * mat4(transformation) );
+    program.bind();
+    mesh.draw();
+    srand(42); // Reset random seed for static colors
+    operations = Operations();
+}
+
 
 Scene::~Scene() {}
