897eafbc21bd4c061928bb96686d6717999f7199
hensing1 <webservices@jhlehmann.de>
2024-07-07 02:06:19 +0200

    HDS Erstellung und Dreiecksunterteilung

--- animationsprojekt/src/classes/halfedge.cpp
+++ animationsprojekt/src/classes/halfedge.cpp
@@ -1,55 +1,142 @@
 #include "halfedge.hpp"
 #include <memory>
+#include <stdexcept>
 
 HDS::HDS(const std::vector<Mesh::VertexPCN>& vertices, const std::vector<unsigned int>& indices) {
     load(vertices, indices);
 }
 
-void HDS::load(const std::vector<Mesh::VertexPCN>& vertices, const std::vector<unsigned int>& indices) {
+void HDS::load(const std::vector<Mesh::VertexPCN>& vertices,
+               const std::vector<unsigned int>& indices) {
     nodes = std::vector<std::shared_ptr<Node>>(vertices.size());
 
-    for (auto& vertex : vertices) {
+    for (size_t i = 0; i < vertices.size(); i++) {
         auto node = std::make_shared<Node>();
-        node->vertex = vertex;
-        node->neighbors = std::unordered_set<unsigned int>();
+        node->id = i;
+        node->vertex = vertices[i];
         nodes.push_back(node);
     }
 
-    for (size_t i = 0; i < indices.size() / 3; i++) {  // drei Vertices pro Fläche
-        auto face = std::make_shared<Face>();
-        auto halfedge1 = std::make_shared<Halfedge>(),
-             halfedge2 = std::make_shared<Halfedge>(),
-             halfedge3 = std::make_shared<Halfedge>();
-        halfedge1->face = face;
-        halfedge2->face = face;
-        halfedge3->face = face;
+    for (size_t i = 0; i < indices.size() / 3; i++) { // drei Vertices pro Fläche
+        make_triangle(faces, edges, nodes[3 * i], nodes[3 * i + 1], nodes[3 * i + 2]);
+    }
 
-        halfedge1->next = halfedge2;
-        halfedge2->next = halfedge3;
-        halfedge3->next = halfedge1;
+    // Gegenkanten finden
+    set_twins(edges);
+}
 
-        halfedge1->prev = halfedge3;
-        halfedge2->prev = halfedge1;
-        halfedge3->prev = halfedge2;
-
-        halfedge1->start = nodes[indices[3 * i]];
-        halfedge2->start = nodes[indices[3 * i + 1]];
-        halfedge3->start = nodes[indices[3 * i + 2]];
-
-        nodes[indices[3 * i]]->neighbors.insert(indices[3 * i + 1]);
-        nodes[indices[3 * i]]->neighbors.insert(indices[3 * i + 2]);
-        nodes[indices[3 * i + 1]]->neighbors.insert(indices[3 * i]);
-        nodes[indices[3 * i + 1]]->neighbors.insert(indices[3 * i + 2]);
-        nodes[indices[3 * i + 2]]->neighbors.insert(indices[3 * i + 1]);
-        nodes[indices[3 * i + 2]]->neighbors.insert(indices[3 * i + 2]);
-
-        face->edge = halfedge1;
+void HDS::set_twins(std::vector<std::shared_ptr<Halfedge>> edges) {
+    for (auto edge : edges) {
+        auto startNode = edge->start;
+        auto endNode = edge->next.lock()->start;
+        edge->twin = find_halfedge(endNode, startNode);
     }
 }
 
-HDS HDS::subdivide() {
-    HDS subdivided = HDS();
+std::weak_ptr<HDS::Halfedge> HDS::find_halfedge(std::weak_ptr<Node> from, std::weak_ptr<Node> to) {
+    for (auto edge : to.lock()->outgoing) {
+        if (edge.lock()->next.lock()->start.lock()->id == from.lock()->id) {
+            return edge;
+        }
+    }
+    throw std::invalid_argument("No halfedge found"); // exception thrown because this should never happen in my use case
+}
+
+void HDS::make_triangle(std::vector<std::shared_ptr<Face>>& faceList,
+                        std::vector<std::shared_ptr<Halfedge>>& edgeList,
+                        std::shared_ptr<Node> node1,
+                        std::shared_ptr<Node> node2,
+                        std::shared_ptr<Node> node3) {
+
+    auto face = std::make_shared<Face>();
+
+    auto halfedge1 = std::make_shared<Halfedge>(),
+         halfedge2 = std::make_shared<Halfedge>(),
+         halfedge3 = std::make_shared<Halfedge>();
+
+    face->edge = halfedge1;
+    faceList.push_back(face);
+
+    halfedge1->start = node1;
+    halfedge2->start = node2;
+    halfedge3->start = node3;
+
+    halfedge1->prev = halfedge3;
+    halfedge2->prev = halfedge1;
+    halfedge3->prev = halfedge2;
+
+    halfedge1->next = halfedge2;
+    halfedge2->next = halfedge3;
+    halfedge3->next = halfedge1;
+
+    halfedge1->face = face;
+    halfedge2->face = face;
+    halfedge3->face = face;
+
+    halfedge1->divided = false;
+    halfedge2->divided = false;
+    halfedge3->divided = false;
+
+    edgeList.push_back(halfedge1);
+    edgeList.push_back(halfedge2);
+    edgeList.push_back(halfedge3);
+
+    node1->outgoing.push_back(halfedge1);
+    node2->outgoing.push_back(halfedge2);
+    node3->outgoing.push_back(halfedge3);
+}
+
+void HDS::subdivide() {
+    std::vector<std::shared_ptr<Face>> newFaces;
+    std::vector<std::shared_ptr<Halfedge>> newEdges;
+
+    auto halfwayNodes = std::vector<std::vector<unsigned int>>(nodes.size());
+    for (unsigned int i = 0; i < halfwayNodes.size(); i++) {
+        halfwayNodes[i] = std::vector<unsigned int>(nodes.size());
+    }
+    
+    auto is_subdivided = [&](std::shared_ptr<Node> n1, std::shared_ptr<Node> n2) {
+        return halfwayNodes[n1->id][n2->id] != 0;
+    };
+
+    auto make_halfway_node = [&](std::shared_ptr<Node> n1, std::shared_ptr<Node> n2) {
+        auto newNode = std::make_shared<Node>();
+        newNode->id = nodes.size();
+        newNode->vertex = intermediate(n1->vertex, n2->vertex);
+        nodes.push_back(newNode);
+        halfwayNodes[n1->id][n2->id] = halfwayNodes[n2->id][n1->id] = newNode->id;
+        return newNode;
+    };
+
     for (const auto& face : faces) {
 
+        std::shared_ptr<Node> node1 = face->edge.lock()->start.lock(),
+                              node2 = face->edge.lock()->next.lock()->start.lock(),
+                              node3 = face->edge.lock()->next.lock()->next.lock()->start.lock();
+
+        // interpolate points
+        auto node12 = is_subdivided(node1, node2) ?
+                 nodes[halfwayNodes[node1->id][node2->id]] : make_halfway_node(node1, node2),
+             node23 = is_subdivided(node2, node3) ?
+                 nodes[halfwayNodes[node2->id][node3->id]] : make_halfway_node(node2, node3),
+             node31 = is_subdivided(node3, node1) ?
+                 nodes[halfwayNodes[node3->id][node1->id]] : make_halfway_node(node3, node1);
+
+
+        make_triangle(newFaces, newEdges, node1, node12, node31);
+        make_triangle(newFaces, newEdges, node12, node2, node23);
+        make_triangle(newFaces, newEdges, node31, node23, node3);
+        make_triangle(newFaces, newEdges, node12, node23, node31);
     }
+
+    set_twins(newEdges);
+
+    edges = newEdges;
+    faces = newFaces;
+}
+
+Mesh::VertexPCN HDS::intermediate(Mesh::VertexPCN& node1, Mesh::VertexPCN& node2) {
+    auto newVertex = Mesh::VertexPCN{};
+    newVertex.position = node1.position + 0.5f * (node2.position - node1.position);
+    return newVertex;
 }
--- animationsprojekt/src/classes/halfedge.hpp
+++ animationsprojekt/src/classes/halfedge.hpp
@@ -6,35 +6,50 @@
  * Halbkanten-Datenstruktur
  */
 class HDS {
-    public:
-        struct Halfedge;
-        struct Face;
-        struct Node;
+  public:
+    struct Halfedge;
+    struct Face;
+    struct Node;
 
-        struct Halfedge{
-            std::weak_ptr<Node> start;
-            std::weak_ptr<Halfedge> prev;
-            std::weak_ptr<Halfedge> next;
-            std::weak_ptr<Halfedge> twin;
-            std::weak_ptr<Face> face;
-        };
-        struct Face {
-            std::weak_ptr<Halfedge> edge;
-        };
-        struct Node {
-            Mesh::VertexPCN vertex;
-            std::unordered_set<unsigned int> neighbors;
-        };
+    struct Halfedge {
+        std::weak_ptr<Node> start;
+        std::weak_ptr<Halfedge> prev;
+        std::weak_ptr<Halfedge> next;
+        std::weak_ptr<Halfedge> twin;
+        std::weak_ptr<Face> face;
+        bool divided;
+    };
+    struct Face {
+        std::weak_ptr<Halfedge> edge;
+    };
+    struct Node {
+        unsigned int id;
+        Mesh::VertexPCN vertex;
+        // std::unordered_set<unsigned int> neighbors;
+        std::vector<std::weak_ptr<Halfedge>> outgoing;
+    };
 
-        HDS();
-        HDS(const std::vector<Mesh::VertexPCN>& vertices, const std::vector<unsigned int>& indices);
-        // void load_mesh(Mesh mesh);
-        void load(const std::vector<Mesh::VertexPCN>& vertices, const std::vector<unsigned int>& indices);
-        Mesh generate_mesh();
-        HDS subdivide();
-    private:
-        std::vector<std::shared_ptr<Node>> nodes;
-        std::vector<std::shared_ptr<Halfedge>> edges;
-        std::vector<std::shared_ptr<Face>> faces;
-        
+    HDS();
+    HDS(const std::vector<Mesh::VertexPCN>& vertices, const std::vector<unsigned int>& indices);
+    // void load_mesh(Mesh mesh);
+    void load(const std::vector<Mesh::VertexPCN>& vertices,
+              const std::vector<unsigned int>& indices);
+
+    void subdivide();
+    Mesh generate_mesh();
+
+  private:
+    std::vector<std::shared_ptr<Node>> nodes;
+    std::vector<std::shared_ptr<Halfedge>> edges;
+    std::vector<std::shared_ptr<Face>> faces;
+
+    void set_twins(std::vector<std::shared_ptr<Halfedge>> edges);
+
+    std::weak_ptr<Halfedge> find_halfedge(std::weak_ptr<Node> from, std::weak_ptr<Node> to);
+
+    void make_triangle(std::vector<std::shared_ptr<Face>>& faceList,
+                       std::vector<std::shared_ptr<Halfedge>>& edgeList, std::shared_ptr<Node> v1,
+                       std::shared_ptr<Node> v2, std::shared_ptr<Node> v3);
+
+    Mesh::VertexPCN intermediate(Mesh::VertexPCN& v1, Mesh::VertexPCN& v2);
 };
