26a65fc14dbe910abdf90615690d5f28ef1166b0
hensing1 <webservices@jhlehmann.de>
2024-07-16 23:30:46 +0200

    Animationsschalter umgelegt

--- animationsprojekt/src/mainapp.cpp
+++ animationsprojekt/src/mainapp.cpp
@@ -41,7 +41,7 @@ MainApp::MainApp() : App(800, 600) {
     FRAME = 0;
     SCENE = prev_scene = 1;
     DEBUG_MODE = false;
-    ANIMATION_PLAYING = false;
+    ANIMATION_PLAYING = true;
 
     App::imguiEnabled = true;
     camera.cartesianPosition = vec3(0.0f, 0.0f, 0.0f);
